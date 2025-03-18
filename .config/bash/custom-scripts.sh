effc65ba3ed477eed5d748ecf696950705279675
knightattheopera <124876442+knightattheopera@users.noreply.github.com>
2025-03-18 16:24:54 +0100

    Add function to change course directory

--- .config/bash/custom-scripts.sh
+++ .config/bash/custom-scripts.sh
@@ -89,3 +89,21 @@ function __custom_script_download_music_playlist_as_mp3 {
 }
 alias yt-mp3=__custom_script_download_music_playlist_as_mp3
 
+# change current course latex notes directory
+function __custom_script_change_current_course_latex_notes {
+	if [ "$#" -eq 2 ]
+	then
+		local latex_notes_path=$HOME/latex-notes/$1/$2
+		if [ -d $latex_notes_path ]
+		then
+			ln -sfn $latex_notes_path/ultisnips/$2.snippets ~/.vim/ultisnips-snippets/tex/current
+			cd $latex_notes_path
+		else
+			echo "course or semester not found"
+			echo "Usage: switch-notes <semester-code> <course-code>"
+		fi
+	else
+		echo "Usage: switch-notes <semester-code> <course-code>"
+	fi
+}
+alias switch-notes=__custom_script_change_current_course_latex_notes

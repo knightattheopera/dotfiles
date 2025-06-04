45543c33490f19300411f977cf59a8ea7ea0abed
knightattheopera <124876442+knightattheopera@users.noreply.github.com>
2025-06-04 16:42:44 +0200

    Fix aliases

--- .config/bash/aliases.sh
+++ .config/bash/aliases.sh
@@ -8,7 +8,8 @@ function __custom_alias_run_zathura { apx run zathura "${1}" 2>/dev/null || zath
 alias zth=__custom_alias_run_zathura 
 function __custom_alias_run_vscode { apx run code "${1}" 2>/dev/null || code "${1}"; }
 alias code=__custom_alias_run_vscode
-alias wat2wasm=~/external-repos/wabt/build/wat2wasm
+alias wat2wasm=~/development/wabt/build/wat2wasm
+alias cs=~/development/coursier/cs
 
 # filesystem navigation
 alias tree1="tree -L 1"
